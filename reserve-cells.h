c592119f4576793086d6e83e38e9441f27395217
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-02-15 21:01:32 -0500

    Create reserve-cells.h

---
+++ reserve-cells.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+void reserve-cells(cellno) {
+     unsigned short int *i = malloc(cellno * sizeof *i); // Define unsigned short integer pointer i, allocate X amount of cells the size of a unsigned+short+int pointer    
+}
