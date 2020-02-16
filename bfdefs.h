5b7aa5a7410f3f93a95a7b91e7335ed012b69f68
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-02-15 21:34:46 -0500

    Create bfdefs.h

---
+++ bfdefs.h
@@ -0,0 +1,11 @@
+unsigned short int *i =
+
+void donothing();
+void set_number_after_use();
+void move_ptr_rt();
+void move_ptr_lt();
+void inc_ptr();
+void dec_ptr();
+void prt_ptr();
+void cond_1();
+void cond_2();
